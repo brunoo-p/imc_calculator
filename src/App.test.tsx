d2c6fa4f2ea585a25e2954dfa0d54be74988e59b
Bruno <brun.op@hotmail.com>
2022-05-21 12:31:43 -0300

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Root', () => {
+
+  test('render root', () => {
+    render(<App />);
+    const rootDom = screen.getByTestId("root");
+    expect(rootDom).toBeInTheDocument();
+  });
+  
 });
